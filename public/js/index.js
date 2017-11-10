3fccbc2be096adf1ffbef56cb4ca1063f6845032
Raju Malla <irwifi@gmail.com>
2017-11-10 22:55:14 +0545

    Notification level. Alert Sound

--- public/js/index.js
+++ public/js/index.js
@@ -6,7 +6,8 @@ $(() => {
     $(".notify_sidebar_body").mCustomScrollbar({theme:"dark", setWidth: "340px"});
 
     $(".notify_trigger button").on("click", () => {
-        trigger_notify(1, "Here is the Notification Message. Press OK to close.", "Notification Title");
+        let level = $('.notify_level_select input:checked').val();
+        trigger_notify(level, "Here is the Notification Message. Press OK to close.", "Notification Title");
     });
 });
 
@@ -30,7 +31,7 @@ let trigger_notify = (level, message, title) => {
     $('#dialog_modal' + max_id).modal();
     $('#dialog_modal' + max_id).on('hidden.bs.modal', function (e) {$(this).remove();});
 
-    $("#notify_alert_sample").attr({"data-max_id": max_id}).clone().attr({"id": "notify_alert" + max_id}).addClass("new").appendTo("#mCSB_1_container").show();
+    $("#notify_alert_sample").attr({"data-max_id": max_id}).clone().attr({"id": "notify_alert" + max_id}).addClass("new notify_alert_level"+level).appendTo("#mCSB_1_container").show();
     $('#notify_alert' + max_id + " .notify_alert_header").text(title);
     $('#notify_alert' + max_id + " .notify_alert_body").text(message);
 
@@ -40,12 +41,21 @@ let trigger_notify = (level, message, title) => {
     });
 
     $(".notify_alert.new").removeClass("new");
+
+    play_audio(level);
+};
+
+let play_audio = (level) => {
+   let audio = new Audio('public/audio/alert' + level + '.mp3');
+    audio.play();
 };
 
 let on_top_event = () => {
     $(".notify_sidebar_header span:not(.active)").on("click", () => {
         $(".notify_sidebar_header span").toggleClass("active").off();
         on_top_event();
+
+        // if(oldest_top)
     });
 };
 })();
